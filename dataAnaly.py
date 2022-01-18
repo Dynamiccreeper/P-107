48e61bbea36724f13cb595e0635fed1ec0072f77
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2022-01-18 17:59:56 +0530

    Update dataAnaly.py

--- dataAnaly.py
+++ dataAnaly.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 df = pd.read_csv("data.csv")
 
 print(df.groupby("level")["attempt"].mean())
-
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
 fig = go.Figure(go.Bar(
             x=df.groupby("level")["attempt"].mean(),
             y=['Level 1', 'Level 2', 'Level 3', 'Level 4'],
